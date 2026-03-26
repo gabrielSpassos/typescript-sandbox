9ab93224e56fbbc9ddf14973ff7dc572b49ce04e
gabrielSpassos <gabrielsantos45725@gmail.com>
2026-03-26 08:42:44 -0300

    feat: update opentui poc

--- opentui-poc/index.ts
+++ opentui-poc/index.ts
@@ -1,7 +1,8 @@
-import { createCliRenderer, Box, Text } from "@opentui/core"
+import { createCliRenderer, Box, Text, BoxRenderable, TextRenderable, InputRenderable, RenderableEvents } from "@opentui/core"
 
 const renderer = await createCliRenderer({
   exitOnCtrlC: true,
+  targetFps: 30,
 })
 
 renderer.root.add(
@@ -11,3 +12,42 @@ renderer.root.add(
     Text({ content: "Press Ctrl+C to exit" }),
   ),
 )
+
+const input = Box(
+    { borderStyle: "rounded", padding: 1, flexDirection: "column", gap: 1 },
+    new InputRenderable(renderer, { id: "username", placeholder: "Enter username...", }),
+)
+
+renderer.root.add(input)
+
+// Give focus to the input
+input.focus()
+
+// Remove focus
+input.blur()
+
+input.on(RenderableEvents.FOCUSED, () => {
+  console.log("Input focused")
+})
+
+input.on(RenderableEvents.BLURRED, () => {
+  console.log("Input blurred")
+})
+
+const button = new BoxRenderable(renderer, {
+  id: "button",
+  border: true,
+  onMouseDown: (event) => {
+    console.log("Clicked at", event.x, event.y)
+  },
+  onMouseOver: (event) => {
+    button.borderColor = "#FFFF00"
+  },
+  onMouseOut: (event) => {
+    button.borderColor = "#FFFFFF"
+  },
+})
+
+button.add(Text({ content: "Click me" }))
+
+renderer.root.add(button)
